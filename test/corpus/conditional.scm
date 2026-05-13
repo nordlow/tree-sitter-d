c4c586ef582516185015b15bb0902de8fd33611b
Per Nordlöw <per.nordlow@gmail.com>
2026-05-13 14:47:35 +0200

    *** empty log message ***

--- test/corpus/conditional.scm
+++ test/corpus/conditional.scm
@@ -27,7 +27,7 @@ unittest
           (equal_binary_expression
             (identifier)
             (integer_literal)))
-        (constant_identifier
+        (constant_declaration
           (constant_declarator
             (constant_identifier)
             (integer_literal)))
